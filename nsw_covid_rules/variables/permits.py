dd62ec7885b0128a11b3d6e22657e60837c78cca
Asghar Kazi <asghar.kazi@gmail.com>
2021-08-30 08:37:06 +1000

    Added permit conditions

--- nsw_covid_rules/variables/permits.py
+++ nsw_covid_rules/variables/permits.py
@@ -1,4 +1,7 @@
 from openfisca_core.indexed_enums import Enum
+from openfisca_core.model_api import *
+from openfisca_nsw_base.entities import *
+from openfisca_core.variables import Variable
 
 # details 1. eligibility and reasons to get a permit to leave Greater Sydney,
 # 2. requirements to get such permit,
@@ -9,10 +12,10 @@ from openfisca_core.indexed_enums import Enum
 class PermitReasonsGreaterSydney(Enum):
     leaving_Greater_Sydney = 'Person is leaving Greater Sydney.'
     relocating_new_place_of_residence = 'Person is relocating to a new place' \
-                                        ' of residence outside Greater Sydney.'
+                                        'of residence outside Greater Sydney.'  # 5.4(1)(a)(i)
     move_between_place_of_residence_for_work = 'Person moves between places of' \
                                                ' residence for work, where only' \
-                                               ' the person moves to the other place of residence.'
+                                               ' the person moves to the other place of residence.'  # 5.4(1)(a)(ii)
     move_between_place_of_residence_for_repairs = 'Person moves between places of' \
                                                   ' residence for urgent maintenance or repairs.'
     move_between_place_of_residence_for_animal_welfare = 'Person moves between places of' \
@@ -20,8 +23,60 @@ class PermitReasonsGreaterSydney(Enum):
     inspect_new_place_of_residence = 'Person is inspecting residential property' \
                                      ' as potential place of residence, with the ' \
                                      ' intent to reside outside Greater Sydney as soon' \
-                                     ' as is practicable.'
+                                     ' as is practicable.'  # 5.4(1)(a)(iii)
     carrying_out_work = 'Person is carrying out work more than 50 kilometres' \
-                        ' from Greater Sydney.'
+                        'from Greater Sydney.'  # 5.4(1)(b)
     carrying_out_emergency_work = 'Person is carrying out work involving the provision of an emergency service.'
     no_valid_reason = 'Person does not have a valid reason to leave Greater Sydney.'
+
+
+class PermitReasonsGreaterSydney(Variable):
+    value_type = Enum
+    entity = Person
+    definition_period = ETERNITY
+    possible_values = PermitReasonsGreaterSydney
+    default_value = PermitReasonsGreaterSydney.no_valid_reason
+    label = 'Permitted reasons for leaving Greater Sydney'
+
+
+class is_eligible_to_be_nominated_person(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = ETERNITY
+    label = 'Person is eligible to be a nominated visitor for another person and' \
+            'may apply for a Service NSW issued permit'
+
+    def formula(persons, period, parameters):
+        return (
+            person('noPersonNominated', period)
+            and person('notNomatedForAnotherPerson', period)
+            and person('livingWithinFiveKm', period)
+            )
+
+
+class noPersonNominated(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = DAY
+    label = 'No other individual has been a nominated visitor for the person'
+
+
+class notNomatedForAnotherPerson(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = DAY
+    label = 'Individual has not been a nominated visitor for another person'
+
+
+class livingWithinFiveKm(Variable):
+    value_type = int
+    entity = Person
+    definition_period = DAY
+    label = 'Nominated individual resides within 5km of the place of residence'
+
+
+class is_nominated_person(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = DAY
+    label = 'The person has been issued a nominated person permit by Service NSW'
