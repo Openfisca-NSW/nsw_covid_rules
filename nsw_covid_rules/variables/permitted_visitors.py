7c83a903f78301959de7f3469a45bb813177fbed
Asghar Kazi <asghar.kazi@gmail.com>
2021-09-07 17:00:24 +1000

    refactored variables and added conditions for workers

--- nsw_covid_rules/variables/permitted_visitors.py
+++ nsw_covid_rules/variables/permitted_visitors.py
@@ -26,14 +26,66 @@ class visitors_permitted_for_stay_at_home_area(Variable):
     entity = Person
     definition_period = ETERNITY
     label = 'Is  person is authorised to visit a place of residence in an area'\
-            'of concern?'
+            'of concern? (Prescribed work that is necessary OR Unprescribed work'\
+            'have no limits to number of workers, but prescribed work has as limit'\
+            'on the number of workers)'
 
     def formula(persons, period, parameters):
         return select(
             [persons('visiting_for_work', period),
             persons('visiting_for_non_work_activities', period)],
-            [persons('is_prescribed_work_necessary', period),
-            persons('is_permitted_for_non_work_activities', period)])
+            [persons('is_prescribed_work_necessary', period) + persons('workers_allowed_at_premises_in_stay_at_home_area', period),
+            persons('permitted_for_non_work_activities_in_stay_at_home_area', period)])
+
+
+class number_of_workers_doing_prescribed_work_in_outdoor_area_stay_at_home_area(Variable):
+    value_type = int
+    entity = Person
+    definition_period = ETERNITY
+    label = 'Number of workers doing prescribed work in  an outdoor area of the'\
+            'the place of residence at any 1 time in a stay at home area'
+
+
+class number_of_workers_doing_prescribed_work_in_indoor_area_within_stay_at_home_area(Variable):
+    value_type = int
+    entity = Person
+    definition_period = ETERNITY
+    label = 'Number of workers doing prescribed work in  an outdoor area of the'\
+            'the place of residence at any 1 time in a stay at home area'
+
+
+class workers_allowed_at_premises_in_stay_at_home_area(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = ETERNITY
+    label = 'Number of workers allowed at premises'
+
+    def formula(persons, period, parameters):
+        return
+        ((number_of_workers_doing_prescribed_work_in_indoor_area_within_stay_at_home_area <= 2)
+        * (number_of_workers_doing_prescribed_work_in_outdoor_area_stay_at_home_area <= 5))
+
+
+class visitors_permitted_for_general_area(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = ETERNITY
+    label = 'Is  person permitted to visit a place of residence in a general'\
+            'area?'
+
+    def formula(persons, period, parameters):
+        return True
+
+
+class visitors_permitted_for_area_of_concern(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = ETERNITY
+    label = 'Is  person is authorised to visit a place of residence in an area'\
+            'of concern?'
+
+    def formula(persons, period, parameters):
+        return True
 
 
 class visiting_for_work(Variable):
@@ -50,7 +102,7 @@ class visiting_for_non_work_activities(Variable):
     label = 'Is  the person visiting for non-work related (other) activities?'
 
 
-class is_permitted_for_non_work_activities(Variable):
+class permitted_for_non_work_activities_in_stay_at_home_area(Variable):
     value_type = bool
     entity = Person
     definition_period = ETERNITY
@@ -81,14 +133,6 @@ class nominated_individual_resides_in_stay_at_home_area(Variable):
     definition_period = ETERNITY
     label = 'Is  the nominated visitor the individual residing in a stay at home'\
             'area but not in an area of concern?'
-
-
-class visitors_permitted_for_general_area(Variable):
-    value_type = bool
-    entity = Person
-    definition_period = ETERNITY
-    label = 'Is  person authorised to visit a place of residence in a general'\
-            'area'
 
 
 class visitors_permitted_for_areas_of_concern(Variable):
@@ -137,7 +181,7 @@ class is_prescribed_work_necessary(Variable):
     value_type = bool
     entity = Person
     definition_period = ETERNITY
-    label = 'Is the prescribed work necessary to be carried out?'
+    label = 'Is the prescribed work necessary to be carried out? (Content needs to be spelled out for defining prescribed work)'
 
     def formula(persons, period, parameters):
         is_prescribed_work = persons('is_prescribed_work', period)
